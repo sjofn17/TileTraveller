770db160e1ffb3cfafe4489bb159109fc7997c6d
sjofn17 <54945684+sjofn17@users.noreply.github.com>
2020-09-17 14:15:18 +0000

    Update tile_traveller.py

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,62 +1,86 @@
-#First write a function that is simple, for example only returns the new position on the board, the rest
-#will be done outside of functions. A loop will be used that prints out the positions on the board based on
-#where the function tells the user he is positioned on the board. If and else statements will be used to decide
-#whether or not no run the function or tell the user he chose an invalid direction.
+NORTH = 'n'
+EAST = 'e'
+SOUTH = 's'
+WEST = 'w'
 
+def move(direction, col, row):
+"Returns updated col, row given the direction"
+    if direction == NORTH:
+        row += 1
+    elif direction == SOUTH:
+        row -= 1
+    elif direction == EAST:
+        col += 1
+    elif direction == WEST:
+        col -= 1
+    return(col, row)    
 
-N = '(N)orth'
-E = '(E)ast'
-S = '(S)outh'
-W = '(W)est'
+def is_victory(col, row):
+"Return true is player is in the victory cell"
+    return col == 3 and row == 1 # (3,1)
 
-x_position, y_position = 1,1 #first position
-available_moves = ('')
-direction = ''
+def print_directions(directions_str):
+    print("You can travel: ", end='')
+    first = True
+    for ch in directions_str:
+        if not first:
+            print(" or ", end='')
+        if ch == NORTH:
+            print("(N)orth", end='')
+        elif ch == EAST:
+            print("(E)ast", end='')
+        elif ch == SOUTH:
+            print("(S)outh", end='')
+        elif ch == WEST:
+            print("(W)est", end='')
+        first = False
+    print(".")
+        
+def find_directions(col, row):
+"Returns valid directions as a string given the supplied location"
+    if col == 1 and row == 1:   # (1,1)
+        valid_directions = NORTH
+    elif col == 1 and row == 2: # (1,2)
+        valid_directions = NORTH+EAST+SOUTH
+    elif col == 1 and row == 3: # (1,3)
+        valid_directions = EAST+SOUTH
+    elif col == 2 and row == 1: # (2,1)
+        valid_directions = NORTH
+    elif col == 2 and row == 2: # (2,2)
+        valid_directions = SOUTH+WEST
+    elif col == 2 and row == 3: # (2,3)
+        valid_directions = EAST+WEST
+    elif col == 3 and row == 2: # (3,2)
+        valid_directions = NORTH+SOUTH
+    elif col == 3 and row == 3: # (3,3)
+        valid_directions = SOUTH+WEST
+    return valid_directions
 
-#I write a program that takes in the previous position, current direction and returns new position
-
-def new_position(x_position, y_position, direction):
+def play_one_move(col, row, valid_directions):
+"Plays one move of the game Return if victory has been obtained and updated col,row "
+    victory = False
+    direction = input("Direction: ")
+    direction = direction.lower()
     
-#I work with the positions (numbers) on the board
-#I use variable.lower to make the program case insensitive
+    if not direction in valid_directions:
+        print("Not a valid direction!")
+    else:
+        col, row = move(direction, col, row)
+        victory = is_victory(col, row)
+    return victory, col, row
 
-    if direction.lower() == 'n': #if 'n'/'N' user moves up one tile, x axis stays the same
-        return x_position, y_position + 1 #function returns new position on the board
-    elif direction.lower() == 'e': #if 'e'/'E' user moves one tile to the right, y stays the same
-        return x_position + 1, y_position 
-    elif direction.lower() == 's': #if south I move down one tile, x axis stays the same...
-        return x_position, y_position - 1
-    elif direction.lower() == 'w':
-        return x_position - 1, y_position
+# The main program starts here
+victory = False
+row = 1
+col = 1
 
-while True:  #runs while move is acceptable, otherwise it prints 'not a valid direction'
-    if direction.lower() in available_moves:
-        if ((x_position == 1) and (y_position == 1)) or ((x_position == 2) and (y_position == 1)):
-            print('You can travel: (N)orth.') #The tiles the only direction you can travel north are 1,1 + 2,1
-            available_moves = ('n') #Prints out the next moves available
-        if (x_position == 1) and (y_position == 2):#Works for tile 1,2
-            print('You can travel: (N)orth or (E)ast or (S)outh.') 
-            available_moves = ('n', 'e', 's') 
-        if (x_position == 1) and (y_position == 3):#1,3
-            print('You can travel: (E)ast or (S)outh.')
-            available_moves = ('e', 's') 
-        if ((x_position == 2) and (y_position == 2)) or ((x_position == 3) and (y_position == 3)):#2,2 + 3,3
-            print('You can travel: (S)outh or (W)est.')
-            available_moves = ('s', 'w') 
-        if (x_position == 2) and (y_position == 3): #2,3
-            print('You can travel: (E)ast or (W)est.')
-            available_moves = ('e', 'w') 
-        if (x_position == 3) and (y_position == 2):
-            print('You can travel: (N)orth or (S)outh.')
-            available_moves = ('n', 's') 
-        if x_position == 3 and y_position == 1:#if position 3,1, then victory
-            print ('Victory!')
-            exit() #stop running program
- #all moves accounted for
-            
-    direction = str(input('Direction: ')) 
-    if direction.lower() in available_moves: #ef direction er available fer forritið í gegn um fallið new_position
-        x_position, y_position = new_position(x_position, y_position, direction)
-        #The function takes in x and y position as well as direction, returns new position
-    else: #If not an available move the program prints out 'Not a valid direction' and returns to the previous position
-        print ('Not a valid direction!')
+valid_directions = NORTH
+print_directions(valid_directions)
+
+while not victory:
+    victory, col, row = play_one_move(col, row, valid_directions)
+    if victory:
+        print("Victory!")
+    else:
+        valid_directions = find_directions(col, row)
+        print_directions(valid_directions)
