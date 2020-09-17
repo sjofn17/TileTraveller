e4def0ce913ec7ecdb1d90caee56c6edf467c216
sjofn17 <54945684+sjofn17@users.noreply.github.com>
2020-09-17 14:20:24 +0000

    Update tile_traveller.py

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,22 +1,23 @@
-NORTH = 'n'
-EAST = 'e'
-SOUTH = 's'
-WEST = 'w'
+#https://github.com/sjofn17/TileTraveller/blob/master/tile_traveller.py
+N = 'n'
+E = 'e'
+S = 's'
+W = 'w'
 
 def move(direction, col, row):
-"Returns updated col, row given the direction"
-    if direction == NORTH:
+#Returns updated col, row given the direction
+    if direction == N:
         row += 1
-    elif direction == SOUTH:
+    elif direction == S:
         row -= 1
-    elif direction == EAST:
+    elif direction == E:
         col += 1
-    elif direction == WEST:
+    elif direction == W:
         col -= 1
     return(col, row)    
 
 def is_victory(col, row):
-"Return true is player is in the victory cell"
+#Return true is player is in the victory cell
     return col == 3 and row == 1 # (3,1)
 
 def print_directions(directions_str):
@@ -25,39 +26,39 @@ def print_directions(directions_str):
     for ch in directions_str:
         if not first:
             print(" or ", end='')
-        if ch == NORTH:
+        if ch == N:
             print("(N)orth", end='')
-        elif ch == EAST:
+        elif ch == E:
             print("(E)ast", end='')
-        elif ch == SOUTH:
+        elif ch == S:
             print("(S)outh", end='')
-        elif ch == WEST:
+        elif ch == W:
             print("(W)est", end='')
         first = False
     print(".")
         
 def find_directions(col, row):
-"Returns valid directions as a string given the supplied location"
+#Returns valid directions as a string given the supplied location
     if col == 1 and row == 1:   # (1,1)
-        valid_directions = NORTH
+        valid_directions = N
     elif col == 1 and row == 2: # (1,2)
-        valid_directions = NORTH+EAST+SOUTH
+        valid_directions = N+E+S
     elif col == 1 and row == 3: # (1,3)
-        valid_directions = EAST+SOUTH
+        valid_directions = E+S
     elif col == 2 and row == 1: # (2,1)
-        valid_directions = NORTH
+        valid_directions = N
     elif col == 2 and row == 2: # (2,2)
-        valid_directions = SOUTH+WEST
+        valid_directions = S+W
     elif col == 2 and row == 3: # (2,3)
-        valid_directions = EAST+WEST
+        valid_directions = E+W
     elif col == 3 and row == 2: # (3,2)
-        valid_directions = NORTH+SOUTH
+        valid_directions = N+S
     elif col == 3 and row == 3: # (3,3)
-        valid_directions = SOUTH+WEST
+        valid_directions = S+W
     return valid_directions
 
 def play_one_move(col, row, valid_directions):
-"Plays one move of the game Return if victory has been obtained and updated col,row "
+#Plays one move of the game Return if victory has been obtained and updated col,row
     victory = False
     direction = input("Direction: ")
     direction = direction.lower()
@@ -74,7 +75,7 @@ victory = False
 row = 1
 col = 1
 
-valid_directions = NORTH
+valid_directions = N
 print_directions(valid_directions)
 
 while not victory:
